05f233c51cd8dd99a3bed6d878697ed6f47cbff9
Pincodeert <leonie.pin@gmail.com>
2022-09-15 22:12:08 +0200

    data.sql uitgebreid met data subscriptions

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,3 +11,12 @@ VALUES
 ('Smiling Lady', 'hay', 'shavings', 'Dr Dre', 'Willemstad', '0655667788'),
 ('Jeremy', 'oats', 'straw', 'Dr Alban', 'Seattle', '0613572468'),
 ('Black', 'oats', 'straw', 'Dr Alban', 'Seattle', '0613572468');
+
+INSERT INTO subscriptions(price, type_of_care, type_of_stall)
+VALUES
+(490.95, 'basic care', 'small inside box'),
+(515.95, 'extra care', 'small inside box'),
+(505.95, 'basic care', 'large inside box'),
+(530.95, 'extra care', 'large inside box'),
+(454.95, 'basic care', 'small outside box'),
+(479.95, 'extra care', 'large outside box');
